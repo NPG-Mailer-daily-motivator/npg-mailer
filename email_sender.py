1b63579d1f5a6e629f2298f48e0a6871b58aecdf
Kacper Jarzyna <81363717+Kacper0202@users.noreply.github.com>
2021-06-15 12:43:58 +0200

    Dodanie funkcjonalności wprowadzenia własnej godziny oraz e-maila

--- email_sender.py
+++ email_sender.py
@@ -4,11 +4,14 @@ from bs4 import BeautifulSoup
 import random
 import schedule
 import time
+import sys
 
+mail = str(sys.argv[1])
+godzina = str(sys.argv[2])
 
 def job():
-    czas = str(time.strftime("%Y-%m-%d", time.localtime()) )
-    
+    czas = str(time.strftime("%Y-%m-%d", time.localtime()))
+
     ilosc_linii = sum(1 for line in open('zlote-mysli.txt'))
 
     linia = random.randint(0, ilosc_linii)
@@ -23,7 +26,6 @@ def job():
     for lin in mysli_plik_linie:
         mysli_plik_wrt.write(lin)
     mysli_plik_wrt.close()
-
 
     url1 = 'https://www.coindesk.com/price/bitcoin'
     url2 = 'https://finance.yahoo.com/quote/OTGLF/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJaDrr-Iotdmj928Ob6Fv40VQIjNirHQlPbzWcRqbFzjqgU6Zve906qozRuGgjUC5uwlWpDd2oSP9k7T23c23NA9ukP3d9qFLL8EKa5IMMRmzAoHqgNIO68dTy6Lumv0bPy7SBdErYhU9SsbykES7GcSQpdpT5o5qcGmXqF12OiR'
@@ -43,25 +45,26 @@ def job():
     div2 = body2.find('span', {'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'})
 
     project_red_act_value = div2.get_text()
-    project_red_17Sep_value = 109.00
-    project_red_diff = str(project_red_17Sep_value - float(project_red_act_value))
+    project_red_17sep_value = 109.00
+    project_red_diff = str(project_red_17sep_value - float(project_red_act_value))
 
     contents = [
-        
+
         "Witaj, \n"
         "Twoja złota myśl na dziś: \n",
         random_mysl,
         "\n"
         "Dzisiejsza wartość bitcoina to " + bitcoin,
         "A akcje Cd projekt red to $" + project_red_act_value + " czyli jest o $" + project_red_diff + " mniej od premiery Cyberpunk 2077 \n"
-        "Smacznej Kawusi!"
+                                                                                                       "Smacznej Kawusi!"
     ]
 
-    yagmail.SMTP('npg.daily.motivator@gmail.com').send('npg.daily.motivator@gmail.com', 'Twój daily motivator - '+ czas, contents)
+
+    yagmail.SMTP('npg.daily.motivator@gmail.com').send(mail,'Twój daily motivator - ' + czas, contents)
 
     return
 
-schedule.every().day.at("08:00").do(job)
+schedule.every().day.at(godzina).do(job)
 
 while True:
     schedule.run_pending()
